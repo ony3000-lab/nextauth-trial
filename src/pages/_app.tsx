44499181188349e8977998020709cda8ba2b672b
Hyeonjong <nianelo4@gmail.com>
2024-08-31 12:52:54 +0900

    Update index page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,22 @@
 import '@/styles/globals.css';
 
+import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 
 import { AppMain } from '@/components';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <RecoilRoot>
-      <AppMain>
-        <Component {...pageProps} />
-      </AppMain>
-    </RecoilRoot>
+    <SessionProvider session={session}>
+      <RecoilRoot>
+        <AppMain>
+          <Component {...pageProps} />
+        </AppMain>
+      </RecoilRoot>
+    </SessionProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import { useSession, signIn, signOut } from 'next-auth/react';
 import Head from 'next/head';
-
-import { BoxLink, Callout, NextByVercel } from '@/components';
+import { Button } from 'tailwind-joy/components';
 
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
     <>
       <Head>
@@ -24,43 +26,17 @@ export default function Home() {
         className="flex min-h-screen flex-col gap-8 px-5 py-8 sm:gap-16 sm:px-12
           sm:py-16 lg:gap-24 lg:px-20 lg:py-24"
       >
-        <NextByVercel />
-
-        <div className="flex justify-center font-mono text-[14px]">
-          <Callout>
-            Get started by editing{' '}
-            <code className="font-bold">src/pages/index.tsx</code>
-          </Callout>
-        </div>
-
-        <div
-          className="mx-auto grid max-w-[1100px] grid-cols-1 sm:grid-cols-2
-            lg:grid-cols-4"
-        >
-          <BoxLink
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            title="Docs"
-            description="Find in-depth information about Next.js features and API."
-          />
-
-          <BoxLink
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            title="Learn"
-            description="Learn about Next.js in an interactive course with quizzes!"
-          />
-
-          <BoxLink
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            title="Templates"
-            description="Discover and deploy boilerplate example Next.js projects."
-          />
-
-          <BoxLink
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            title="Deploy"
-            description="Instantly deploy your Next.js site to a shareable URL with Vercel."
-          />
-        </div>
+        {session ? (
+          <>
+            Signed in as {session.user.email} <br />
+            <Button onClick={() => signOut()}>Sign out</Button>
+          </>
+        ) : (
+          <>
+            Not signed in <br />
+            <Button onClick={() => signIn()}>Sign in</Button>
+          </>
+        )}
       </div>
     </>
   );
