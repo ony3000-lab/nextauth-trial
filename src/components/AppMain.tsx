fd296de00c68fc6b0d4ffa0e3d8bd9cf58c6bd14
Hyeonjong <nianelo4@gmail.com>
2024-08-31 19:22:48 +0900

    Remove UI flickering that can occur when session is active

--- src/components/AppMain.tsx
+++ src/components/AppMain.tsx
@@ -1,9 +1,16 @@
+import { useSession } from 'next-auth/react';
 import type { ComponentProps } from 'react';
 
 import { classNames } from '@/adaptors';
 import { pretendard } from '@/fonts';
 
 export default function AppMain({ children }: ComponentProps<'main'>) {
+  const { status } = useSession();
+
+  if (status === 'loading') {
+    return null;
+  }
+
   return (
     <main className={classNames(pretendard.variable, 'font-sans')}>
       {children}
