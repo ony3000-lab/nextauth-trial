f336940ed458d143c57a3b406a7f9ff2e0624a86
Hyeonjong <nianelo4@gmail.com>
2025-04-04 23:44:48 +0900

    Update API route

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,29 @@
+import jwt from 'jsonwebtoken';
 import type { AuthOptions } from 'next-auth';
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
+
+const secret = 'my-256-bit-secret';
+const expiresInSeconds = 300;
 
 async function login(username: string, password: string) {
   if (username !== 'Bret' || password !== 'qwer1234') {
     throw new Error('User not exist');
   }
 
+  const token = jwt.sign(
+    {
+      name: 'John Doe',
+    },
+    secret,
+    {
+      expiresIn: `${expiresInSeconds}s`,
+    },
+  );
+
   return {
     id: '12345',
-    access:
-      'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NSIsIm5hbWUiOiJKb2huIERvZSIsImlhdCI6MTc0MzY4ODgwMCwiZXhwIjoxNzQzNzMyMDAwfQ.kVsp7-7qJU5do0oQcGEWvdC36WSLVhEsVESz67eNvqw',
+    access: token,
   };
 }
 
@@ -51,6 +64,41 @@ export const authOptions: AuthOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async jwt({ token, user, account, profile, isNewUser }) {
+      // 로그인할 때, 또는 클라이언트에서 세션에 접근할 때마다 호출된다.
+      // `token`을 제외한 나머지 파라미터는 이 콜백이 처음 호출될 때(=로그인할 때)만 존재할 수 있으며, 이후 호출에서는 `token`만 존재한다.
+      // 따라서 파라미터에 저장된 데이터를 유지하고 싶은 경우, 첫 번째 호출에서 `token`에 데이터를 추가해야 한다.
+
+      if (user) {
+        /* eslint-disable no-param-reassign */
+
+        // 여기에서 `user`는 `CredentialsProvider`의 `authorize`에서 반환한 값이다.
+
+        // @ts-expect-error
+        token.access = user.access;
+
+        /* eslint-enable no-param-reassign */
+      }
+
+      return token;
+    },
+    async session({ session, user, token }) {
+      // 세션을 검사할 때마다 호출된다.
+      // `jwt` 콜백에서 `token`에 추가한 데이터를 클라이언트에서 사용할 수 있게 하려면, 이 콜백에서 `session`에 데이터를 추가해야 한다.
+
+      /* eslint-disable no-param-reassign */
+
+      // 여기에서 `token`은 `jwt` 콜백(두 번째 호출부터)에서 반환한 값이다.
+
+      // @ts-expect-error
+      session.access = token.access;
+
+      /* eslint-enable no-param-reassign */
+
+      return session;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
